21a7704add67ef6e62eae1b53e5bdd458b32b3a6
pjaeyoung <devdodose@gmail.com>
2021-01-12 14:14:01 +0900

    feat: css 추가

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 import { initializeApollo } from '../lib/apolloClient';
 import { todos } from '../gql';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ todos }) {
   return (
-    <main>
-      <form>
-        <input type="text" name="todo 입력" />
-      </form>
-      <ul>
-        {todos.map(({ id, content }) => (
-          <li key={id} data-id={id}>
-            {content}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className={styles.container}>
+      <header className={styles.title}>TodoList</header>
+      <main className={styles.main}>
+        <form>
+          <input
+            className={styles.input}
+            type="text"
+            name="todo 입력"
+            placeholder="새 할 일을 입력하세요...."
+          />
+        </form>
+        <ul className={styles.grid}>
+          {todos.map(({ id, content }) => (
+            <li className={styles.card} key={id} data-id={id}>
+              {content}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
