ba68e82defcf27bc8479fcd9623ebcada4a4ed82
pjaeyoung <devdodose@gmail.com>
2021-01-12 13:54:32 +0900

    feat : todos 받아와 렌더링

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div></div>;
+import { initializeApollo } from '../lib/apolloClient';
+import { todos } from '../gql';
+
+export default function Home({ todos }) {
+  return (
+    <main>
+      <form>
+        <input type="text" name="todo 입력" />
+      </form>
+      <ul>
+        {todos.map(({ id, content }) => (
+          <li key={id} data-id={id}>
+            {content}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+  const { data } = await apolloClient.query({
+    query: todos,
+  });
+
+  return { props: { todos: data.todos }, revalidate: 1 };
 }
