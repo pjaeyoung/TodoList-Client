0301981dd57d6e46c72a32d8e497f734bea80996
pjaeyoung <devdodose@gmail.com>
2021-01-18 23:02:14 +0900

    refactor : updateTodo, removeTodo Todos 컴포넌트로 옮기기

--- components/Todos.js
+++ components/Todos.js
@@ -1,11 +1,56 @@
+import { useMutation } from '@apollo/client';
+import { REMOVE_TODO, UPDATE_TODO } from '@gql';
 import { Todo } from '@components';
 import styles from 'styles/Home.module.css';
 
-export default function Todos({
-  todos,
-  onClickRemoveButton,
-  onClickUpdateButton,
-}) {
+export default function Todos({ todos, setTodos }) {
+  const [removeTodo] = useMutation(REMOVE_TODO, {
+    update: (cache, { data: { removeTodo: removeId } }) => {
+      cache.modify({
+        fields: {
+          todo(cachedTodos) {
+            return cachedTodos.filter((todo) => todo.id !== removeId);
+          },
+        },
+      });
+      setTodos((prev) => prev.filter((todo) => todo.id !== removeId));
+    },
+  });
+  const onClickRemoveButton = ({
+    target: {
+      dataset: { id },
+    },
+  }) => {
+    removeTodo({ variables: { id } });
+  };
+
+  const [updateTodo] = useMutation(UPDATE_TODO, {
+    update: (cache, { data: { updateTodo: updatedTodo } }) => {
+      cache.modify({
+        fields: {
+          todo(cachedTodos) {
+            const updatedTodoIndex = cachedTodos.findIndex(
+              ({ id }) => updatedTodo.id === id,
+            );
+            cachedTodos[updatedTodoIndex] = updatedTodo;
+            return cachedTodos;
+          },
+        },
+      });
+      setTodos((prev) => {
+        const updatedTodoIndex = prev.findIndex(
+          ({ id }) => updatedTodo.id === id,
+        );
+        prev[updatedTodoIndex] = updatedTodo;
+        return prev;
+      });
+    },
+  });
+
+  const onClickUpdateButton = ({ id, content }) => {
+    updateTodo({ variables: { id, content } });
+  };
+
   return (
     <ul className={styles.grid}>
       {todos.map(({ id, content }) => (
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useMutation } from '@apollo/client';
 import { initializeApollo } from '@lib/apolloClient';
-import { TODOS, REMOVE_TODO, UPDATE_TODO } from '@gql';
+import { TODOS } from '@gql';
 import styles from 'styles/Home.module.css';
 import { Todos, TodoInput } from '@components';
 
@@ -13,63 +12,12 @@ Home.propTypes = {
 export default function Home({ todos: initialTodos }) {
   const [todos, setTodos] = useState(initialTodos);
 
-  const [removeTodo] = useMutation(REMOVE_TODO, {
-    update: (cache, { data: { removeTodo: removeId } }) => {
-      cache.modify({
-        fields: {
-          todo(cachedTodos) {
-            return cachedTodos.filter((todo) => todo.id !== removeId);
-          },
-        },
-      });
-      setTodos((prev) => prev.filter((todo) => todo.id !== removeId));
-    },
-  });
-  const onClickRemoveButton = ({
-    target: {
-      dataset: { id },
-    },
-  }) => {
-    removeTodo({ variables: { id } });
-  };
-
-  const [updateTodo] = useMutation(UPDATE_TODO, {
-    update: (cache, { data: { updateTodo: updatedTodo } }) => {
-      cache.modify({
-        fields: {
-          todo(cachedTodos) {
-            const updatedTodoIndex = cachedTodos.findIndex(
-              ({ id }) => updatedTodo.id === id,
-            );
-            cachedTodos[updatedTodoIndex] = updatedTodo;
-            return cachedTodos;
-          },
-        },
-      });
-      setTodos((prev) => {
-        const updatedTodoIndex = prev.findIndex(
-          ({ id }) => updatedTodo.id === id,
-        );
-        prev[updatedTodoIndex] = updatedTodo;
-        return prev;
-      });
-    },
-  });
-
-  const onClickUpdateButton = ({ id, content }) => {
-    updateTodo({ variables: { id, content } });
-  };
-
   return (
     <div className={styles.container}>
       <header className={styles.title}>TodoList</header>
       <main className={styles.main}>
         <TodoInput setTodos={setTodos} />
-        <Todos
-          todos={todos}
-          onClickRemoveButton={onClickRemoveButton}
-          onClickUpdateButton={onClickUpdateButton}
-        />
+        <Todos todos={todos} setTodos={setTodos} />
       </main>
     </div>
   );
