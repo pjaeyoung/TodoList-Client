4630749947c48cbe92b0a6188967e92cd28f0cae
pjaeyoung <devdodose@gmail.com>
2021-01-19 11:14:43 +0900

    style : TodoInput ts 변환

---
+++ components/TodoInput.tsx
@@ -0,0 +1,57 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_TODO } from '@gql';
+import styles from 'styles/Home.module.css';
+import { Todo } from '@types';
+
+interface TodoInputProps {
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
+}
+
+const TodoInput: React.FC<TodoInputProps> = ({ setTodos }) => {
+  const [addTodo] = useMutation<{ addTodo: Todo }, { content: string }>(
+    ADD_TODO,
+    {
+      update: (cache, { data: { addTodo: newTodo } }) => {
+        cache.modify({
+          id: cache.identify(newTodo),
+          fields: {
+            todo(cachedTodos: Todo[]) {
+              return [...cachedTodos, newTodo];
+            },
+          },
+        });
+        setTodos((prev) => [...prev, newTodo]);
+      },
+    },
+  );
+  const [content, setContent] = useState<string>('');
+  const onSubmitTodo = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    addTodo({ variables: { content } });
+    setContent('');
+  };
+
+  const onChangeTodo = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const {
+      target: { value },
+    } = e;
+    setContent(value);
+  };
+
+  return (
+    <form onSubmit={onSubmitTodo}>
+      <input
+        className={styles.input}
+        type='text'
+        name='todo 입력'
+        placeholder='새 할 일을 입력하세요....'
+        value={content}
+        onChange={onChangeTodo}
+        autoComplete='off'
+      />
+    </form>
+  );
+};
+
+export default TodoInput;
